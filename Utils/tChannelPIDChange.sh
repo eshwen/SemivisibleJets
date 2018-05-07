f1bf27e15292962e23be5b581167810f3d5df9cf
Eshwen <eshwen.bhal@bristol.ac.uk>
2018-05-07 11:46:40 +0200

    Moved tChannelPIDChange script to Utils

---
+++ Utils/tChannelPIDChange.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+    echo "------------------------------------------------
+Usage: ./tChannelPIDChange.sh <path to LHE file>
+------------------------------------------------"
+fi
+
+lheFile=$1
+
+sed -i 's/49001010/4900101/g' $lheFile
+sed -i 's/49001011/4900101/g' $lheFile
+sed -i 's/49001012/4900101/g' $lheFile
+sed -i 's/49001013/4900101/g' $lheFile
+sed -i 's/49001014/4900101/g' $lheFile
+
+exit
