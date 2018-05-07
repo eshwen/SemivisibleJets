be5e6f4691522692fabc59248107870f4a7c7614
Eshwen <eshwen.bhal@bristol.ac.uk>
2018-05-07 14:04:47 +0200

    Added print statements in tChannelPIDChange. Fixed  naming convention in s-channel model MG input files. Fixed coupling orders bug where it contained the .pyo file interspersed in the .py file

--- Utils/tChannelPIDChange.sh
+++ Utils/tChannelPIDChange.sh
@@ -8,10 +8,14 @@ fi
 
 lheFile=$1
 
+echo "Changing PIDs for t-channel..."
+
 sed -i 's/49001010/4900101/g' $lheFile
 sed -i 's/49001011/4900101/g' $lheFile
 sed -i 's/49001012/4900101/g' $lheFile
 sed -i 's/49001013/4900101/g' $lheFile
 sed -i 's/49001014/4900101/g' $lheFile
+
+echo "Done!"
 
 exit
