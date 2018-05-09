6fd7690e5e9d94865a858d1c7eea5d906aebf46e
Eshwen <eshwen.bhal@bristol.ac.uk>
2018-05-09 11:14:36 +0200

    Completed runLHERetrieval script. It runs the script generated with the gridpack to get the LHE file out, splits it, then transfers the output files to where the user specified

--- LHE_from_Gridpack/runLHERetrieval.py
+++ LHE_from_Gridpack/runLHERetrieval.py
@@ -1,8 +1,11 @@
 import argparse
+import glob
 import os
 import pprint
+import random
 import re
 import shutil
+from splitLHE import splitLHE
 from subprocess import call
 import yaml
 
@@ -22,19 +25,23 @@ def main():
 
     print "The arguments you have set are the following:\n", pprint.pprint(input_params)
 
-    work_space = input_params['work_space']
     model_name = input_params['model_name']
     total_events = input_params['total_events']
+    n_jobs = input_params['n_jobs']
+    split_lhe_file_path = input_params['lhe_file_path']
 
-
-    default_gridpack_dir = os.path.join(os.environ['MG_GENPROD_DIR'], model_name)
+    # Set up some path variables for use later
+    svj_top_dir = os.environ['SVJ_TOP_DIR']
+    genprod_dir = os.environ['MG_GENPROD_DIR']
+    default_gridpack_dir = os.path.join(genprod_dir, model_name)
+    lhe_gen_dir = os.path.join(default_gridpack_dir, model_name+'_gridpack', 'work', 'gridpack')
 
     # Get cross section from gridpack generation log file
-    log_file = open(os.path.join(default_gridpack_dir, model_name+'_gridpack', 'work', 'gridpack', 'gridpack_generation.log'), 'r')
+    log_file = open(os.path.join(lhe_gen_dir, 'gridpack_generation.log'), 'r')
     log_str = log_file.read()
     x_sec = re.search("(?<=Cross-section :   )(\d*.\d+)", log_str).group(0)
     log_file.close()
-    
+
     # Append cross section to config file if not included already
     config_read = open(args.config, 'r')
     config_orig_str = config_read.read()
@@ -58,7 +65,37 @@ def main():
         config_append.close()
 
 
-    # COPY GRIDPACK TARBALL TO gridpacks/, RUN runcmsgrid.sh WITH N_EVENTS AND RANDOM SEED ARGUMENTS (FIND FUNCTION TO CREATE RANDOM INTEGER) ON UNTARRED VERSION TO GET LHE FILE, MOVE LHE FILE SOMEWHERE AND DELETE UNTARRED GRIDPACK,  THEN SPLIT LHE FILE AND STORE IN DIRECTORY SPECIFIED BY USER
+    # Copy gridpack tarball to gridpacks/
+    for tarball in glob.glob( os.path.join(genprod_dir, model_name+'*.xz') ):
+        print "Copying {0} to gridpacks/".format( os.path.basename(tarball) )
+        shutil.copy(tarball, os.path.join(svj_top_dir, 'gridpacks'))
+        print "Copied gridpack to", os.path.join(svj_top_dir, 'gridpacks')
+
+    # Run the script produced with the gridpack to get the LHE file out and copy to gridpacks/ dir
+    random_seed = random.randint(0, 1000000)
+    call("cd {0}; ./runcmsgrid.sh {1} {2}".format(lhe_gen_dir, total_events, random_seed), shell = True)
+
+    lhe_output_path = os.path.join(svj_top_dir, 'gridpacks', model_name+'_LHE.lhe')
+    shutil.copy( os.path.join(lhe_gen_dir, 'cmsgrid_final.lhe'), lhe_output_path )
+
+    # Delete untarred gridpack as it takes up unnecessary space
+    shutil.rmtree(default_gridpack_dir)
+    print "Removed untarred version of gridpack!"
+
+
+    # Split the LHE file, the output files being stored in the current directory
+    print "Splitting LHE file..."
+    splitLHE(inputFile=lhe_output_path, outFileNameBase=model_name+'_split', numFiles=n_jobs)
+    os.remove(lhe_output_path)
+
+    # Copy the split LHE files to directory specified by user
+    if not os.path.exists(split_lhe_file_path):
+        os.mkdir(split_lhe_file_path)
+    for splitFile in glob.glob( os.path.join(os.getcwd(), model_name+'_split*.lhe') ):
+        shutil.copy(splitFile, split_lhe_file_path)
+        os.remove(splitFile)
+    print "Split LHE files copied to", split_lhe_file_path
+
 
 
 if __name__ == '__main__':
