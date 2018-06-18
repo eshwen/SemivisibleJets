0c28b54b8554ce2a0c862dc7b6be743ca41aecf3
Eshwen <eshwen.bhal@bristol.ac.uk>
2018-06-18 14:18:26 +0200

    Added colours to some print statements in runFullSim_condor.sh. Also increased spacing between some commands for easier reading

--- FullSim_files/Condor/runFullSim_condor.sh
+++ FullSim_files/Condor/runFullSim_condor.sh
@@ -28,7 +28,6 @@ fi
 # Write so CMSSW versions aren't hardcoded, but can take from dict in submitFullSim_condor.py or something
 cd CMSSW_7_1_30/src
 cmsenv
-scram b
 
 
 # Run the cmsDriver and cmsRun commands for each step in the chain
@@ -39,35 +38,39 @@ cmsDriver.py Configuration/GenProduction/python/${gen_frag_file} --filein file:$
 sed -i "s/process.options = cms.untracked.PSet(/process.options = cms.untracked.PSet(SkipEvent = cms.untracked.vstring(\'ProductNotFound\'),/g" ${model_name}_GEN_SIM_${seed}.py
 
 cmsRun ${model_name}_GEN_SIM_${seed}.py
-echo "**** CREATED GEN-SIM FILE ****"
+echo -e "\e[1;35m**** CREATED GEN-SIM FILE ****\e[0m"
 
 mv ${model_name}_GEN_SIM_${seed}.root ../../CMSSW_8_0_21/src/
 cd ../../CMSSW_8_0_21/src
 cmsenv
 
 cmsDriver.py step1 --filein file:${model_name}_GEN_SIM_${seed}.root --fileout file:${model_name}_AOD_step1_${seed}.root --pileup_input filelist:${SVJ_TOP_DIR}/pileup_filelist.txt --mc --eventcontent PREMIXRAW --datatier GEN-SIM-RAW --conditions 80X_mcRun2_asymptotic_2016_TrancheIV_v6 --step DIGIPREMIX_S2,DATAMIX,L1,DIGI2RAW,HLT:@frozen2016 --datamix PreMix --era Run2_2016 --python_filename ${model_name}_AOD_step1_${seed}.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n $n_events
+
 cmsRun ${model_name}_AOD_step1_${seed}.py
-echo "**** CREATED AOD (STEP 1) FILE ****"
+echo -e "\e[1;35m**** CREATED AOD (STEP 1) FILE ****\e[0m"
 
 cmsDriver.py step2 --filein file:${model_name}_AOD_step1_${seed}.root --fileout file:${model_name}_AOD_step2_${seed}.root --mc --eventcontent AODSIM --runUnscheduled --datatier AODSIM --conditions 80X_mcRun2_asymptotic_2016_TrancheIV_v6 --step RAW2DIGI,RECO,EI --era Run2_2016 --python_filename ${model_name}_AOD_step2_${seed}.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n $n_events
+
 cmsRun ${model_name}_AOD_step2_${seed}.py
-echo "**** CREATED AOD (STEP 2) FILE ****"
+echo -e "\e[1;35m**** CREATED AOD (STEP 2) FILE ****\e[0m"
 
 cmsDriver.py --filein file:${model_name}_AOD_step2_${seed}.root --fileout file:${model_name}_MINIAOD_${seed}.root --mc --eventcontent MINIAODSIM --runUnscheduled --datatier MINIAODSIM --conditions 80X_mcRun2_asymptotic_2016_TrancheIV_v6 --step PAT --era Run2_2016 --python_filename ${model_name}_MINIAOD_${seed}.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n $n_events
+
 cmsRun ${model_name}_MINIAOD_${seed}.py
-echo "**** CREATED MINIAOD FILE ****"
+echo -e "\e[1;35m**** CREATED MINIAOD FILE ****\e[0m"
 
 mv ${model_name}_MINIAOD_${seed}.root ../../CMSSW_9_4_4/src/
 cd ../../CMSSW_9_4_4/src
 cmsenv
 
 cmsDriver.py --filein file:${model_name}_MINIAOD_${seed}.root --fileout file:${model_name}_NANOAOD_${seed}.root --mc --eventcontent NANOAODSIM --datatier NANOAODSIM --conditions auto:run2_mc -s NANO --era Run2_2016,run2_miniAOD_80XLegacy --python_filename ${model_name}_NANOAOD_${seed}.py --no_exec -n $n_events
+
 cmsRun ${model_name}_NANOAOD_${seed}.py
-echo "**** CREATED NANOAOD FILE ****"
+echo -e "\e[1;35m**** CREATED NANOAOD FILE ****\e[0m"
 
 mv ${model_name}_NANOAOD_${seed}.root $work_space/output/
 
-echo "**** CLEANING UNNECESSARY FILES ****"
+echo -e "\e[1;36m**** CLEANING UNNECESSARY FILES ****\e[0m"
 rm $work_space/CMSSW_*/src/${model_name}_{GEN_SIM,AOD_step1,AOD_step2,MINIAOD}_${seed}.{py,root}
 
 exit
