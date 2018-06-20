8865a585b4ec5ecce9efdf75afe7dffba04472f6
Eshwen <eshwen.bhal@bristol.ac.uk>
2018-06-20 13:05:46 +0200

    Added colours to raise error statements. Removed deprecated function

--- Utils/checkConfig.py
+++ Utils/checkConfig.py
@@ -20,26 +20,17 @@ def performBasicChecks(configDict):
     r_inv = configDict['r_inv']
 
     if not all (type(i) is int for i in [n_events, n_jobs, m_med, m_d]):
-        raise TypeError('{0} is required to be an integer.'.format(i))
+        raise TypeError(Fore.GREEN + 'n_events, n_jobs, m_med and m_d are all required to be integers.')
     if m_d < 0 or m_med < 0:
-        raise ValueError('m_d and m_med must be positive.')
+        raise ValueError(Fore.GREEN + 'm_d and m_med must be positive.')
     if m_med < 2*m_d:
-        raise ValueError('m_med must be greater than 2*m_d for on-shell pair production of the dark quarks.')
+        raise ValueError(Fore.GREEN + 'm_med must be greater than 2*m_d for on-shell pair production of the dark quarks.')
     if not 's-channel' in process_type and not 't-channel' in process_type:
-        raise ValueError('Unknown process_type specified. Please either specify \'s-channel\' or \'t-channel\'.')
+        raise ValueError(Fore.GREEN + 'Unknown process_type specified. Please either specify \'s-channel\' or \'t-channel\'.')
     if r_inv < 0 or r_inv > 1.0:
-        raise ValueError('r_inv is required to be in the range 0 < r_inv < 1.0.')
+        raise ValueError(Fore.GREEN + 'r_inv is required to be in the range 0 < r_inv < 1.0.')
     else:
         print Fore.MAGENTA + "Basic config file check finished. All looks good!", Style.RESET_ALL
-
-
-def loadConfig(configFile):
-    """
-    Load the config file into a dictionary.
-    """
-
-    configDict = yaml.load( open(configFile, 'r') )
-    return configDict
 
 
 def performThoroughChecks(configDict):
@@ -55,16 +46,15 @@ def performThoroughChecks(configDict):
     x_sec = configDict['x_sec']
 
     if not os.path.exists(lhe_file_path):
-        raise ValueError('The LHE file path you have specified does not exist.')
+        raise ValueError(Fore.GREEN + 'The LHE file path you have specified does not exist.')
     if not type(n_f) is int:
-        raise TypeError('n_f is required to be an integer.')
+        raise TypeError(Fore.GREEN + 'n_f is required to be an integer.')
     if x_sec < 0.0 or alpha_d < 0.0:
-        raise ValueError('x_sec and alpha_d must be positive.')
+        raise ValueError(Fore.GREEN + 'x_sec and alpha_d must be positive.')
     else:
         print Fore.MAGENTA + "Thorough config file check finished. All looks good!", Style.RESET_ALL
 
         
-
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
