b6d45b0033448e1094a409829c4d7ebb39f59573
Eshwen <eshwen.bhal@bristol.ac.uk>
2018-06-24 17:55:50 +0200

    Added starting file number in case command gets interrupted

--- FullSim_files/Condor/runNanoAOD_Kevin_samples_local.sh
+++ FullSim_files/Condor/runNanoAOD_Kevin_samples_local.sh
@@ -9,4 +9,7 @@ n_events=500
 
 python writers/write_combine_script.py -w $work_space -m $model
 
-for seed in $(seq 1 1 $n_files); do seed=$(echo $seed | bc); ./runNanoAOD_Kevin_samples.sh $work_space root://cmseos.fnal.gov///store/user/lpcsusyhad/SVJ2017/ProductionV2/MINIAOD/${model}_part-${seed}.root $model $n_events $seed; done
+# Starting file should be 1 if running from beginning. But if command gets interrupted, check most recent file created and go from there
+starting_file=59
+
+for seed in $(seq $starting_file 1 $n_files); do seed=$(echo $seed | bc); ./runNanoAOD_Kevin_samples.sh $work_space root://cmseos.fnal.gov///store/user/lpcsusyhad/SVJ2017/ProductionV2/MINIAOD/${model}_part-${seed}.root $model $n_events $seed; done
