39695e845a48b8b31236041fedf7bcdebc210052
Eshwen <eshwen.bhal@bristol.ac.uk>
2019-02-25 19:07:27 +0100

    Moved xrd path to own variable

--- FullSim_files/Condor/runNanoAOD_Kevin_samples_local.sh
+++ FullSim_files/Condor/runNanoAOD_Kevin_samples_local.sh
@@ -2,10 +2,11 @@
 
 # Run Kevin's miniAODs through to nanoAOD locally. Re-run if command gets interrupted. Checks should take care of everything
 
-work_space=/afs/cern.ch/work/e/ebhal/Semi_visible_jets_Condor_v6
+work_space="/afs/cern.ch/work/e/ebhal/Semi_visible_jets_Condor_Kevin"
+ext_dir="root://cmseos.fnal.gov///store/user/lpcsusyhad/SVJ2017/ProductionV3/2017/MINIAOD"
 model="step4_MINIAOD_mZprime-1000_mDark-20_rinv-0.3_alpha-0.2_n-1000"
-n_files=200
-n_events=250
+n_files=100
+n_events=1000
 
 python writers/write_combine_script.py -w $work_space -m $model
 
@@ -17,4 +18,4 @@ else
     starting_file=1
 fi
 
-for seed in $(seq $starting_file 1 $n_files); do seed=$(echo $seed | bc); ./runNanoAOD_Kevin_samples.sh $work_space root://cmseos.fnal.gov///store/user/lpcsusyhad/SVJ2017/ProductionV2/MINIAOD/${model}_part-${seed}.root $model $n_events $seed; done
+for seed in $(seq $starting_file 1 $n_files); do seed=$(echo $seed | bc); ./runNanoAOD_Kevin_samples.sh $work_space ${ext_dir}/${model}_part-${seed}.root $model $n_events $seed; done
