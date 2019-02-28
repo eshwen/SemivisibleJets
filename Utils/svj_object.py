0b7908ac9a63ece275942628d18c0f46ab47bf91
Eshwen <eshwen.bhal@bristol.ac.uk>
2019-02-28 16:58:53 +0100

    WIP: Adding SVJ class for easier I/O handling in the different stages

---
+++ Utils/svj_object.py
@@ -0,0 +1,31 @@
+from loadYamlConfig import loadYamlConfig
+
+
+class SVJObject(object):
+    def __init__(self, config):
+        self.config = config
+        self.in_params = loadYamlConfig(self.config)
+        self.n_jobs = in_params['n_jobs']
+
+    def gridpack_stage(self):
+        self.process_type = self.in_params['process_type']
+        self.m_med = self.in_params['m_med']
+        self.n_events = self.in_params['n_events']
+        self.m_d = self.in_params['m_d']
+        self.r_inv = self.in_params['r_inv']
+
+    def lhe_stage(self):
+        self.model_name = self.in_params['model_name']
+        self.total_events = self.in_params['total_events']
+        self.split_lhe_file_path = self.in_params['lhe_file_path']
+
+    def full_sim_stage(self):
+        self.work_space = self.in_params['work_space']
+        self.lhe_file_path = self.in_params['lhe_file_path']
+        self.n_events = self.in_params['n_events']
+        self.model_name = self.in_params['model_name']
+        self.alpha_d = self.in_params['alpha_d']
+        self.m_d = self.in_params['m_d']
+        self.n_f = self.in_params['n_f']
+
+# FINISH
