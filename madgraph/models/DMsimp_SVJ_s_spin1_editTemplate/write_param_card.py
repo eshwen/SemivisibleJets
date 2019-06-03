65761088b5b8642c87b4164cad9003ba1148fa14
Eshwen <eshwen.bhal@bristol.ac.uk>
2019-06-03 13:28:22 +0200

    Removed some redundant blank lines. Made stuff in main more robust

--- madgraph/models/DMsimp_SVJ_s_spin1_editTemplate/write_param_card.py
+++ madgraph/models/DMsimp_SVJ_s_spin1_editTemplate/write_param_card.py
@@ -1,8 +1,9 @@
-
+import os
 __date__ = "02 Aug 2012"
 __author__ = 'olivier.mattelaer@uclouvain.be'
 
 from function_library import *
+
 
 class ParamCardWriter(object):
     
@@ -29,6 +30,7 @@ class ParamCardWriter(object):
         
         self.write_card(list_of_parameters)
         self.fsock.close()
+
     
     def define_not_dep_param(self, list_of_parameters):
         """define self.dep_mass and self.dep_width in case that they are 
@@ -41,11 +43,11 @@ class ParamCardWriter(object):
         self.dep_width = [(part, part.width) for part in all_particles\
                              if part.pdg_code > 0 and \
                                 part.width not in list_of_parameters]        
+
     
     @staticmethod
     def order_param(obj1, obj2):
         """ order parameter of a given block """
-        
         maxlen = min([len(obj1.lhacode), len(obj2.lhacode)])
     
         for i in range(maxlen):
@@ -62,10 +64,10 @@ class ParamCardWriter(object):
             return 0
         else:
             return -1
+
         
     def write_card(self, all_ext_param):
         """ """
-        
         # list all lhablock
         all_lhablock = set([param.lhablock for param in all_ext_param])
         
@@ -91,10 +93,10 @@ class ParamCardWriter(object):
 
         if self.generic_output:
             self.write_qnumber()
+
                                
     def write_block(self, name):
         """ write a comment for a block"""
-        
         self.fsock.writelines(
         """\n###################################""" + \
         """\n## INFORMATION FOR %s""" % name.upper() +\
@@ -103,8 +105,8 @@ class ParamCardWriter(object):
         if name!='DECAY':
             self.fsock.write("""Block %s \n""" % name)
 
+
     def write_param(self, param, lhablock):
-        
         lhacode=' '.join(['%3s' % key for key in param.lhacode])
         if lhablock != 'DECAY':
             text = """  %s %e # %s \n""" % (lhacode, complex(param.value).real, param.name ) 
@@ -112,8 +114,6 @@ class ParamCardWriter(object):
             text = '''DECAY %s %e \n''' % (lhacode, complex(param.value).real)
         self.fsock.write(text) 
                     
-
-
     
     def write_dep_param_block(self, lhablock):
         import cmath
@@ -162,9 +162,6 @@ class ParamCardWriter(object):
                 text += """%s %s %f # %s : %s \n""" %(prefix, particle.pdg_code, 
                         value, particle.name, name)
 
-
-
-
         self.fsock.write(text)    
         
     sm_pdg = [1,2,3,4,5,6,11,12,13,13,14,15,16,21,22,23,24,25]
@@ -173,6 +170,7 @@ class ParamCardWriter(object):
         2 %(spin)d  # number of spin states (2S+1)
         3 %(color)d  # colour rep (1: singlet, 3: triplet, 8: octet)
         4 %(antipart)d  # Particle/Antiparticle distinction (0=own anti)\n"""
+
     
     def write_qnumber(self):
         """ write qnumber """
@@ -194,14 +192,10 @@ class ParamCardWriter(object):
                                  'antipart': part.name != part.antiname and 1 or 0}
         
         self.fsock.write(text)
-        
-            
-            
-            
-            
-        
-            
+
+
+
 if '__main__' == __name__:
-    ParamCardWriter('./param_card.dat', generic=True)
-    print 'write ./param_card.dat'
-    
+    out_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'param_card.dat')
+    ParamCardWriter(out_file, generic=True)
+    print 'Written', out_file
