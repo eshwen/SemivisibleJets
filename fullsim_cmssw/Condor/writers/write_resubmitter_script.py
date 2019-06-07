23798b9f9636e5d255064230d528d0e396a74256
Eshwen <eshwen.bhal@bristol.ac.uk>
2019-06-07 13:09:09 +0200

    Reshuffled so I can import primary function in other scripts. Tidied other code

--- fullsim_cmssw/Condor/writers/write_resubmitter_script.py
+++ fullsim_cmssw/Condor/writers/write_resubmitter_script.py
@@ -5,17 +5,9 @@ from colorama import Fore, Style
 import os
 from subprocess import call
 
-parser = ArgumentParser(description=__doc__, formatter_class=ArgumentDefaultsHelpFormatter)
-parser.add_argument("-w", "--work_space", type=str, required=True, help="Work space containing CMSSW releases, input and output files")
-parser.add_argument("-m", "--model_name", type=str, required=True, help="Identifying name of model")
-parser.add_argument("-n", "--n_jobs", type=int, required=True, help="Number of jobs to be submitted")
-args = parser.parse_args()
 
-
-def main():
-    work_space = args.work_space
-    model_name = args.model_name
-    n_jobs_for_loop = args.n_jobs - 1
+def write_resubmitter_script(work_space, model_name, n_jobs):
+    n_jobs_for_loop = n_jobs - 1
 
     file_path = os.path.join(work_space, "resubmit_{}.sh".format(model_name))
     # Write bash combine script
@@ -24,13 +16,13 @@ def main():
 # Resubmit failed jobs by running this script. It checks to see if the output nanoAOD file is present for each seed.
 # Note that this should only be performed when all jobs have finished running.
 : "${{SVJ_TOP_DIR:?Please source the setup script before running this as environment variables are required.}}; exit"
-    for i in $(seq 0 1 {n_jobs}); do
+    for i in $(seq 0 1 {n_jobs_for_loop}); do
     if [ ! -r {work_space}/output/{model}_NANOAOD_$i.root ]; then
         echo "Found no output file for {model} with seed $i. Resubmitting..."
         condor_submit -batch-name {model} {work_space}/submission_scripts/{model}/condor_submission_$i.job
     fi
 done
-""".format(n_jobs=n_jobs_for_loop, work_space=work_space, model=model_name)
+""".format(n_jobs_for_loop=n_jobs_for_loop, work_space=work_space, model=model_name)
         )
 
     call("chmod +x {}".format(file_path), shell=True)
@@ -38,4 +30,10 @@ done
 
 
 if __name__ == '__main__':
-    main()
+    parser = ArgumentParser(description=__doc__, formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument("work_space", type=str, help="Work space containing CMSSW releases, input and output files")
+    parser.add_argument("model_name", type=str, help="Identifying name of model")
+    parser.add_argument("n_jobs", type=int, help="Number of jobs to be submitted")
+    args = parser.parse_args()
+
+    write_resubmitter_script(*args)
