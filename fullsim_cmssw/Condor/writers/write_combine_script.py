690a9bf95306b2ad410702ad1fc4413f20adb359
Eshwen <eshwen.bhal@bristol.ac.uk>
2019-06-09 14:53:13 +0200

    Using work_space variable in script to make final .sh file less bloated. Fixed bug in argparse usage

--- fullsim_cmssw/Condor/writers/write_combine_script.py
+++ fullsim_cmssw/Condor/writers/write_combine_script.py
@@ -16,23 +16,24 @@ def write_combine_script(work_space, model_name):
 echo -e "\e[1;33mWarning: May take a while to hadd if many files are present.\e[0m"
 shopt -s expand_aliases
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-cd {work_space}/CMSSW_9_4_4/src # Try to make this not hardcoded in case that version doesn't exist
+work_space="{work_space}"
+cd $work_space/CMSSW_9_4_4/src # Try to make this not hardcoded in case that version doesn't exist
 cmsenv
-cd {work_space}
+cd $work_space
 
 # Capture output of hadding command in file to check for errors
 temp_file=$(mktemp)
 
-{SVJ_top_dir}/utils/haddnano.py {work_space}/output/{model}_nanoAOD_final.root {work_space}/output/{model}*NANOAOD*.root 2>&1 | tee $temp_file
+{SVJ_top_dir}/utils/haddnano.py $work_space/output/{model}_nanoAOD_final.root $work_space/output/{model}*NANOAOD*.root 2>&1 | tee $temp_file
 
-if [ ! -d {work_space}/output/components ]; then
-    mkdir {work_space}/output/components
+if [ ! -d $work_space/output/components ]; then
+    mkdir $work_space/output/components
 fi
 
 if grep -rq "Error in <TFile\|has size 0\|AttributeError\|Traceback" $temp_file > /dev/null; then
     echo -e "\e[1;32mFound an error while combining files. Check the output and try again. If you're struggling to locate the offending file (and getting an error related to missing leaves), try doing `ls -alrth <files>` and check the files with the smallest sizes.\e[0m"
 else
-    mv {work_space}/output/{model}_NANOAOD_*.root {work_space}/output/components
+    mv $work_space/output/{model}_NANOAOD_*.root $work_space/output/components
 fi
 
 exit
@@ -49,4 +50,4 @@ if __name__ == '__main__':
     parser.add_argument("model_name", type=str, help="Identifying name of model")
     args = parser.parse_args()
 
-    write_combine_script(*args)
+    write_combine_script(args.work_space, args.model_name)
