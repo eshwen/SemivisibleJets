d442de2f81b6170ae59a425536ee151114f391e6
Eshwen <eshwen.bhal@bristol.ac.uk>
2019-06-13 15:01:09 +0200

    Renamed top_dir to work_space as it makes more sense

--- fullsim_cmssw/Condor/sourceCMSSW.sh
+++ fullsim_cmssw/Condor/sourceCMSSW.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
-    usr_msg="Usage ./sourceCMSSW.sh CMSSW_VER ARCH TOP_DIR"
+if [ -z $3 ]; then
+    usr_msg="Usage ./sourceCMSSW.sh CMSSW_VER ARCH WORK_SPACE"
     $SVJ_TOP_DIR/utils/print_bash_script_usage.sh "$usr_msg"
     exit
 fi
 
 cmssw_ver=$1
 arch=$2
-top_dir=$(readlink -m $3)
+work_space=$(readlink -m $3)
 
-pushd $top_dir >/dev/null 2>&1
+pushd $work_space >/dev/null 2>&1
 
 # Allow use of aliases (specifically cvmfs ones)
 shopt -s expand_aliases
