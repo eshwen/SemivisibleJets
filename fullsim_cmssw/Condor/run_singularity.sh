0e2031ab332a1022ba7f48c8c9a6e83f7538b283
Eshwen <eshwen.bhal@bristol.ac.uk>
2019-06-13 14:59:45 +0200

    Adding bold to echo statement. Speeding up creation of Docker container by reusing cache directory, if possible

--- fullsim_cmssw/Condor/run_singularity.sh
+++ fullsim_cmssw/Condor/run_singularity.sh
@@ -7,10 +7,17 @@ if [ -z $1 ]; then
 fi
 
 command=$1
-echo "Running singularity to create a Docker container with an SLC6 environment, allowing the execution of the following: $command"
+echo -e "\e[1mRunning singularity to create a Docker container with an SLC6 environment, allowing the execution of the following: $command\e[0m"
 
-cachedir=$(mktemp -d -t sing-XXXX --tmpdir=/tmp/$(whoami)/singularity)
+# Allow reusing of cache dir if it exists. Speeds up creation of Docker container
+cachedir="/tmp/$(whoami)/singularity_$(date +%Y%m%d%H)"
+if [ -d "$cachedir" ]; then
+    :
+else
+    cachedir=$(mkdir -p /tmp/$(whoami)/singularity_$(date +%Y%m%d))
+fi
 export SINGULARITY_CACHEDIR="$cachedir"
+
 # For singularity >3.0
 singularity exec -B /afs,/eos,/cvmfs docker://cmssw/slc6:latest $command
 # For singularity <3.0
