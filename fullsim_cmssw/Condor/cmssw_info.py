084f6db9c5891b2636d8dfc47a2252e3b0a18d61
Eshwen <eshwen.bhal@bristol.ac.uk>
2019-09-04 17:46:43 +0200

    Updated 2016 CMSSW versions to make miniAODv2 and nanoAODv5 files

--- fullsim_cmssw/Condor/cmssw_info.py
+++ fullsim_cmssw/Condor/cmssw_info.py
@@ -13,21 +13,21 @@ class CmsswInfo(object):
 
         if self.year == 2016:
             self.gensim = {'version': 'CMSSW_7_1_38_patch1', 'arch': 'slc6_amd64_gcc481'}  # earlier versions don't have CMSSW plug-ins for dark quark/Z2 filters
-            self.aod = {'version': 'CMSSW_8_0_21', 'arch': 'slc6_amd64_gcc530'}
-            self.nano = {'version': 'CMSSW_9_4_4', 'arch': 'slc6_amd64_gcc630'}
-            self.new_pythia = True  # CMSSW_7_1_38_patch1 ships with old Pythia version
+            self.aod = {'version': 'CMSSW_8_0_31', 'arch': 'slc6_amd64_gcc530'}
+            self.nano = {'version': 'CMSSW_10_2_15_patch2', 'arch': 'slc6_amd64_gcc700'}  # using patch2 because architecture is slc6. don't want to interfere and cause issues if 2017/18 are being run in same work_space
+            self.install_new_pythia = True  # CMSSW_7_1_38_patch1 ships with old Pythia version
 
         elif self.year == 2017:
             self.gensim = {'version': 'CMSSW_9_3_15', 'arch': 'slc7_amd64_gcc630'}  # earlier versions (at least <=9_3_12) don't have CMSSW plug-ins for dark quark/Z2 filters
             self.aod = {'version': 'CMSSW_9_4_10', 'arch': 'slc7_amd64_gcc630'}
             self.nano = {'version': 'CMSSW_10_2_15', 'arch': 'slc7_amd64_gcc700'}
-            self.new_pythia = False  # CMSSW_9_3_15 already ships with Pythia 8.230
+            self.install_new_pythia = False  # CMSSW_9_3_15 already ships with Pythia 8.230
 
         elif self.year == 2018:
             self.gensim = {'version': 'CMSSW_10_2_15', 'arch': 'slc7_amd64_gcc700'}  # earlier versions (at least <=10_2_3) don't have CMSSW plug-ins for dark quark/Z2 filters
             self.aod = {'version': 'CMSSW_10_2_15', 'arch': 'slc7_amd64_gcc700'}
             self.nano = {'version': 'CMSSW_10_2_15', 'arch': 'slc7_amd64_gcc700'}
-            self.new_pythia = False  # CMSSW_10_2_15 already ships with Pythia 8.230
+            self.install_new_pythia = False  # CMSSW_10_2_15 already ships with Pythia 8.230
 
         else:
             sys.exit("Only years 2016, 2017, and 2018 are supported")
@@ -44,7 +44,7 @@ class CmsswInfo(object):
                 run_in_slc6_env(_command, target_arch=stage['arch'])
 
         # Install new Pythia version if not already done so
-        if self.new_pythia:
+        if self.install_new_pythia:
             _command = '{}/sourceNewPythiaVer.sh {} {} {}'.format(this_dir, location, self.gensim['version'], self.gensim['arch'])
             run_in_slc6_env(_command, target_arch=self.gensim['arch'])
 
