895ac35f7f4765ca418c440b2cdb285bd7eaabaa
Eshwen <eshwen.bhal@bristol.ac.uk>
2020-07-04 19:02:28 +0200

    Adding support for a 'v_low' and 'v_high' parameter for specifying alpha_dark. Switching to using numpy over math library

--- utils/calc_dark_params.py
+++ utils/calc_dark_params.py
@@ -1,16 +1,16 @@
 """ Calculates quantities required in semi-visible jet models """
-import math
+import numpy as np
 
 
 def calc_alpha_d(n_c, n_f, Lambda_d):
     b_param = calc_b_param(n_c, n_f)
-    alpha_d = -2.0*math.pi / (b_param * math.log(Lambda_d/1000.0))
+    alpha_d = -2.0*np.pi / (b_param * np.log(Lambda_d/1000.0))
     return alpha_d
 
 
 def calc_lambda_d(n_c, n_f, alpha_d):
     b_param = calc_b_param(n_c, n_f)
-    Lambda_d = 1000.0 * math.exp(-2.0*math.pi / (alpha_d*b_param))
+    Lambda_d = 1000.0 * np.exp(-2.0*np.pi / (alpha_d*b_param))
     return Lambda_d
 
 
@@ -22,17 +22,21 @@ def calc_b_param(n_c, n_f):
 def calc_lambda_d_from_str(n_c, n_f, alpha_d, m_dh):
     if not isinstance(alpha_d, str):
         raise TypeError("alpha_d must be a string")
-    elif not any(alpha_d == x for x in ['peak', 'low', 'high']):
-        raise ValueError("alpha_d must equal 'peak', 'low', or 'high'")
+    sfs = {
+        "v_low": 0.25,
+        "low": 0.5,
+        "peak": 1.,
+        "high": 1.5,
+        "v_high": 2.,
+    }
+    if not any(alpha_d == x for x in sfs.keys()):
+        raise ValueError("alpha_d must equal one of the following: {}".format(sfs.keys()))
+
+    Lambda_d_peak = 3.2 * np.power(m_dh, 0.8)
+    if alpha_d == "peak":
+        Lambda_d = Lambda_d_peak
     else:
-        Lambda_d_peak = 3.2 * math.pow(m_dh, 0.8)
-        if alpha_d == "peak":
-            Lambda_d = Lambda_d_peak
-        else:
-            alpha_d_peak = calc_alpha_d(n_c, n_f, Lambda_d_peak)
-            if alpha_d == "low":
-                a_d = 0.5 * alpha_d_peak
-            elif alpha_d == "high":
-                a_d = 1.5 * alpha_d_peak
-            Lambda_d = calc_lambda_d(n_c, n_f, a_d)
-        return Lambda_d
+        alpha_d_peak = calc_alpha_d(n_c, n_f, Lambda_d_peak)
+        a_d = sfs[alpha_d] * alpha_d_peak
+        Lambda_d = calc_lambda_d(n_c, n_f, a_d)
+    return Lambda_d
